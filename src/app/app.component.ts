397d62dee00026aa91ac921c2e847d527bd819f3
stopnoanime <birecki@proton.me>
2023-01-05 21:18:06 +0100

    Basic POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Compiler, Parser, Sim } from '16ttac-sim';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '16TTAC-web';
+export class AppComponent implements OnInit {
+  outputBuffer = ''
+
+  parser = new Parser();
+  compiler = new Compiler();
+
+  sim = new Sim({
+    outputRawCallback: (n) => this.outputBuffer += String.fromCharCode(n)
+  });
+
+  sourceCode = `'a' => OUT 'v' => OUT 'c' => OUT`
+
+  compile() {
+    this.sim.initializeMemory(this.compiler.compile(this.parser.parse(this.sourceCode)))
+  }
+  
+  ngOnInit() {
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
