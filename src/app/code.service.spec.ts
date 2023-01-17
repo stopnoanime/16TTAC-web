697a50b2b67e7a82997cdfa09027af2984c9aab1
stopnoanime <birecki@proton.me>
2023-01-17 12:16:20 +0100

    Add tests for services

--- src/app/code.service.spec.ts
+++ src/app/code.service.spec.ts
@@ -1,9 +1,12 @@
+import { Parser } from '16ttac-sim';
 import { TestBed } from '@angular/core/testing';
 
 import { CodeService } from './code.service';
 
 describe('CodeService', () => {
   let service: CodeService;
+
+  const parser = new Parser();
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +15,41 @@ describe('CodeService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get source code index range for address', () => {
+    expect(
+      service.getSourceCodeIndexRangeForAddress(
+        0,
+        parser.parse(`NULL=>ACC ACC => PUSH`)
+      )
+    ).toEqual([0, 9]);
+    expect(
+      service.getSourceCodeIndexRangeForAddress(
+        1,
+        parser.parse(`NULL=>ACC ACC => PUSH`)
+      )
+    ).toEqual([10, 21]);
+  });
+
+  it('should get address for source code index', () => {
+    expect(
+      service.getAddressForSourceCodeIndex(
+        10,
+        parser.parse(`NULL=>ACC ACC => PUSH`)
+      )
+    ).toBe(1);
+    expect(
+      service.getAddressForSourceCodeIndex(
+        20,
+        parser.parse(`NULL=>ACC ACC => PUSH`)
+      )
+    ).toBe(1);
+    expect(
+      service.getAddressForSourceCodeIndex(
+        5,
+        parser.parse(`NULL=>ACC ACC => PUSH`)
+      )
+    ).toBe(0);
   });
 });
--- src/app/sim.service.spec.ts
+++ src/app/sim.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { last, take } from 'rxjs';
 
 import { SimService } from './sim.service';
 
@@ -12,5 +13,100 @@ describe('SimService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should output error on bad input', () => {
+    service.outputEvent.subscribe((text) => {
+      expect(text).toContain('Expected');
+    });
+    service.compile('abc');
+  });
+
+  it('should output confirmation on compile', () => {
+    service.outputEvent.subscribe((text) => {
+      expect(text).toContain('Compiled successfully');
+    });
+    service.compile('NULL => NULL');
+  });
+
+  it('should single step', () => {
+    service.compile('123 => PUSH');
+    service.singleStep();
+
+    expect(service.simProp.stackPointer).toBe(1);
+    expect(service.simProp.stack[0]).toBe(123);
+  });
+
+  it('should output halt message on halt', () => {
+    service.outputEvent.pipe(take(2), last()).subscribe((text) => {
+      expect(text).toContain('Halting');
+    });
+
+    service.compile('NULL => HALT');
+    service.singleStep();
+  });
+
+  it('should reset', () => {
+    service.singleStep();
+    service.singleStep();
+    service.reset();
+
+    expect(service.simProp.pc).toBe(0);
+  });
+
+  it('should start', () => {
+    jasmine.clock().install();
+
+    service.timeout = 100;
+    service.compile('123 => PUSH 1234 => PUSH 12345 => PUSH');
+    service.startStop();
+
+    jasmine.clock().tick(200);
+
+    expect(service.simProp.stackPointer).toBe(3);
+    expect([...service.simProp.stack.slice(0, 3)]).toEqual([123, 1234, 12345]);
+
+    jasmine.clock().uninstall();
+  });
+
+  it('should start at full speed', () => {
+    service.compile('123 => PUSH 1234 => PUSH 12345 => PUSH NULL => HALT');
+    service.fullSpeed = true;
+    service.startStop();
+
+    expect(service.simProp.stackPointer).toBe(3);
+    expect([...service.simProp.stack.slice(0, 3)]).toEqual([123, 1234, 12345]);
+  });
+
+  it('should output data', () => {
+    service.outputEvent.pipe(take(2), last()).subscribe((text) => {
+      expect(text).toBe('a');
+    });
+
+    service.compile(`'a' => OUT`);
+    service.singleStep();
+  });
+
+  it('should read input data', () => {
+    service.compile(`IN => ACC`);
+    service.input = 'x';
+    service.singleStep();
+
+    expect(service.simProp.acc).toBe('x'.charCodeAt(0));
+  });
+
+  it('should stop on breakpoint', () => {
+    service.compile(`
+    NULL => NULL
+    NULL => NULL
+    NULL => HALT
+    `);
+    service.fullSpeed = true;
+    service.breakpoints = {
+      1: [22, 34],
+    };
+    service.startStop();
+
+    expect(service.simProp.pc).toBe(1);
   });
 });
